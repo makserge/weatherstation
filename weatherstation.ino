f279d65bad7ea10231d736bce06bf492227518f4
Sergey <makserge@mail.ru>
2017-10-23 23:23:26 +0300

    Bugfixes and black / white icons

--- weatherstation.ino
+++ weatherstation.ino
@@ -48,9 +48,10 @@ const char* WEATHER_CITY_ID = "703448"; //Kiev;
 const char* WEATHER_KEY = "bd5e378503939ddaee76f12ad7a97608";
 const int WEATHER_ATTEMPTS = 10;
 
+const int WEATHER_BUFFER_SIZE = 1536;
+
 const double NTP_SERVER_UPDATE_INTERVAL = 86400;
-const double WEATHER_UPDATE_INTERVAL = 3600;
-const double TEMPERATURE_UPDATE_INTERVAL = 900;
+const double WEATHER_UPDATE_INTERVAL = 30 * 60;
 const double TIME_UPDATE_INTERVAL = 60;
 const double TIME_TICK_UPDATE_INTERVAL = 1;
 
@@ -59,20 +60,17 @@ const int CLOCK_X_POS = 10;
 const int CLOCK_Y_POS = 80;
 const int CLOCK_TICK_X_POS = 74;
 
-const int TEMPERATURE_X_POS = 150;
+const int TEMPERATURE_X_POS = 160;
 const int HUMIDITY_X_POS = 130;
 const int HUMIDITY_Y_POS = 39;
 
 const int INT_TEMPERATURE_X_POS = 120;
 const int INT_TEMPERATURE_Y_POS = 50;
 const int INT_HUMIDITY_X_POS = 130;
-const int INT_HUMIDITY_Y_POS = 55 ;
+const int INT_HUMIDITY_Y_POS = 55;
 
-#define BLACK    0x0000
-#define BLUE     0x001F
-#define YELLOW   0xFFE0 
-#define WHITE    0xFFFF
-#define GREY     0xC618
+unsigned int BLACK = 0x0000;
+unsigned int WHITE = 0xFFFF;
 
 WiFiUDP udp;
 IPAddress ntpServerIP;
@@ -83,9 +81,14 @@ TFT_eSPI tft = TFT_eSPI();
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
+int lastWeatherId;
 int outHumidity;
 float outTemp;
 String weatherId;
+
+float intTemp;
+float intHumidity;
+
 boolean tickShown = false;
 
 boolean isNight = false;
@@ -147,6 +150,7 @@ unsigned long getFromNTP() {
 }
 
 void getWeatherData() {
+  getTemperatureData();
   Serial.print("connecting to "); 
   Serial.println(WEATHER_HOST);
   if (httpClient.connect(WEATHER_HOST, WEATHER_PORT)) {
@@ -158,21 +162,20 @@ void getWeatherData() {
     Serial.println("connection failed");
     return;
   }
-  String line;
   int repeatCounter = 0;
   while (!httpClient.available() && repeatCounter < WEATHER_ATTEMPTS) {
     delay(500);
     repeatCounter++;
   }
+  String weatherData = "";
   while (httpClient.connected() && httpClient.available()) {
     char c = httpClient.read(); 
     if (c == '[' || c == ']') c = ' ';
-    line += c;
+    weatherData += c;
   }
-
   httpClient.stop();
-  DynamicJsonBuffer jsonBuf;
-  JsonObject &root = jsonBuf.parseObject(line);
+  StaticJsonBuffer<WEATHER_BUFFER_SIZE> jsonBuffer;
+  JsonObject &root = jsonBuffer.parseObject(weatherData);
   if (!root.success()) {
     Serial.println("parseObject() failed");
     return;
@@ -180,20 +183,39 @@ void getWeatherData() {
   outTemp = root["main"]["temp"];
   outHumidity = root["main"]["humidity"];
   weatherId = root["weather"]["id"].asString();
+   
+  if (isnan(outTemp) || outTemp < -50 || outTemp > 50) {
+    return;
+  }
 
+  tft.fillRect(86, 0, 160, 50, BLACK);
+
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setFreeFont(&ArialRoundedMTBold_36);
   tft.setTextDatum(TR_DATUM);
   tft.setTextPadding(tft.textWidth("-88`"));
-  tft.drawString(((outTemp > 0) ? "+" : "-") + String((int)outTemp) + "`", TEMPERATURE_X_POS, 0);
+  String temp = "";
+  if ((int)outTemp < 0) {
+    temp = "-";
+  }
+  else if ((int)outTemp > 0) {
+    temp = "+";
+  }
+  temp += String((int)outTemp) + "`";
+  tft.drawString(temp, TEMPERATURE_X_POS, 0);
 
   tft.setCursor(HUMIDITY_X_POS, HUMIDITY_Y_POS);
   tft.setTextFont(0);
   tft.print(String(outHumidity) + "%");
 
-  printWeatherIcon(weatherId.toInt());
+  if (weatherId.toInt() != lastWeatherId) {
+    lastWeatherId = weatherId.toInt();
+    printWeatherIcon(weatherId.toInt());
+  }
 }
 
 void printWeatherIcon(int id) {
+  tft.fillRect(0, 0, 85, CLOCK_Y_POS - 5, BLACK);
   switch(id) {
     case 800: 
       drawClearWeather();
@@ -297,85 +319,85 @@ void drawFewClouds() {
 }
 
 void drawTheSun() {
-   tft.fillCircle(40, 33, 26, YELLOW);
+  tft.fillCircle(40, 33, 26, WHITE);
 }
 
 void drawTheMoon() {
-  tft.fillCircle(40, 33, 26, GREY);
+  tft.fillCircle(40, 33, 26, WHITE);
   tft.fillCircle(51, 26, 26, BLACK);
 }
 
 void drawCloud() {
   tft.drawBitmap(0, 16, cloud, 80, 43, BLACK);
-  tft.drawBitmap(0, 20, cloud, 80, 43, GREY);
+  tft.drawBitmap(0, 20, cloud, 80, 43, WHITE);
 }
 
 void drawCloudWithSun() {
-  tft.fillCircle(55, 25, 20, YELLOW);
+  tft.fillCircle(55, 25, 20, WHITE);
   tft.drawBitmap(0, 16, cloud, 80, 43, BLACK);
-  tft.drawBitmap(0, 20, cloud, 80, 43, GREY);
+  tft.drawBitmap(0, 20, cloud, 80, 43, WHITE);
 }
 
 void drawCloudWithMoon() {
-  tft.fillCircle(72, 25, 18, GREY);
+  tft.fillCircle(72, 25, 18, WHITE);
   tft.fillCircle(83, 18, 18, BLACK);
   tft.drawBitmap(0, 16, cloud, 80, 43, BLACK);
-  tft.drawBitmap(0, 20, cloud, 80, 43, GREY);
+  tft.drawBitmap(0, 20, cloud, 80, 43, WHITE);
 }
 
 void drawThunderstorm() {
-  tft.drawBitmap(0, 5, cloud, 80, 43, GREY);
-  tft.drawBitmap(31, 48, thunder, 16, 19, YELLOW);
-  tft.fillRoundRect(23, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(30, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(49, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(57, 52, 3, 15, 1, BLUE);
+  tft.drawBitmap(0, 5, cloud, 80, 43, WHITE);
+  tft.drawBitmap(31, 48, thunder, 16, 19, WHITE);
+  tft.fillRoundRect(23, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(30, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(49, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(57, 52, 3, 15, 1, WHITE);
 }
 
 void drawLightRain() {
-  tft.drawBitmap(0, 5, cloud, 80, 43, GREY);
-  tft.fillRoundRect(25, 55, 3, 13, 1, BLUE);
-  tft.fillRoundRect(40, 55, 3, 13, 1, BLUE);
-  tft.fillRoundRect(55, 55, 3, 13, 1, BLUE);
+  tft.drawBitmap(0, 5, cloud, 80, 43, WHITE);
+  tft.fillRoundRect(25, 55, 3, 13, 1, WHITE);
+  tft.fillRoundRect(40, 55, 3, 13, 1, WHITE);
+  tft.fillRoundRect(55, 55, 3, 13, 1, WHITE);
 }
 
 void drawModerateRain() {
-  tft.drawBitmap(0, 5, cloud, 80, 43, GREY);
-  tft.fillRoundRect(25, 55, 3, 15, 1, BLUE);
-  tft.fillRoundRect(32, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(40, 55, 3, 15, 1, BLUE);
-  tft.fillRoundRect(47, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(55, 55, 3, 15, 1, BLUE);
+  tft.drawBitmap(0, 5, cloud, 80, 43, WHITE);
+  tft.fillRoundRect(25, 55, 3, 15, 1, WHITE);
+  tft.fillRoundRect(32, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(40, 55, 3, 15, 1, WHITE);
+  tft.fillRoundRect(47, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(55, 55, 3, 15, 1, WHITE);
 }
 
 void drawHeavyRain() {
-  tft.drawBitmap(0, 5, cloud, 80, 43, GREY);
-  tft.fillRoundRect(18, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(25, 55, 3, 15, 1, BLUE);
-  tft.fillRoundRect(32, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(40, 5, 3, 15, 1, BLUE);
-  tft.fillRoundRect(47, 52, 3, 15, 1, BLUE);
-  tft.fillRoundRect(55, 55, 3, 15, 1, BLUE);
-  tft.fillRoundRect(62, 52, 3, 15, 1, BLUE);
+  tft.drawBitmap(0, 5, cloud, 80, 43, WHITE);
+  tft.fillRoundRect(18, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(25, 55, 3, 15, 1, WHITE);
+  tft.fillRoundRect(32, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(40, 5, 3, 15, 1, WHITE);
+  tft.fillRoundRect(47, 52, 3, 15, 1, WHITE);
+  tft.fillRoundRect(55, 55, 3, 15, 1, WHITE);
+  tft.fillRoundRect(62, 52, 3, 15, 1, WHITE);
 }
 
 void drawCloudWithMoonAndRain() {
-  tft.fillCircle(72, 25, 18, GREY);
+  tft.fillCircle(72, 25, 18, WHITE);
   tft.fillCircle(83, 18, 18, BLACK);
   tft.drawBitmap(0, 16, cloud, 80, 43, BLACK);
-  tft.drawBitmap(0, 20, cloud, 80, 43, GREY);
-  tft.fillRoundRect(25, 65, 3, 11, 1, BLUE);
-  tft.fillRoundRect(40, 65, 3, 11, 1, BLUE);
-  tft.fillRoundRect(55, 65, 3, 11, 1, BLUE);
+  tft.drawBitmap(0, 20, cloud, 80, 43, WHITE);
+  tft.fillRoundRect(25, 65, 3, 11, 1, WHITE);
+  tft.fillRoundRect(40, 65, 3, 11, 1, WHITE);
+  tft.fillRoundRect(55, 65, 3, 11, 1, WHITE);
 }
 
 void drawCloudWithSunAndRain() {
-  tft.fillCircle(55, 25, 20, YELLOW);
+  tft.fillCircle(55, 25, 20, WHITE);
   tft.drawBitmap(0, 16, cloud, 80, 43, BLACK);
-  tft.drawBitmap(0, 20, cloud, 80, 43, GREY);
-  tft.fillRoundRect(25, 65, 3, 13, 1, BLUE);
-  tft.fillRoundRect(40, 65, 3, 13, 1, BLUE);
-  tft.fillRoundRect(55, 65, 3, 13, 1, BLUE);
+  tft.drawBitmap(0, 20, cloud, 80, 43, WHITE);
+  tft.fillRoundRect(25, 65, 3, 13, 1, WHITE);
+  tft.fillRoundRect(40, 65, 3, 13, 1, WHITE);
+  tft.fillRoundRect(55, 65, 3, 13, 1, WHITE);
 }
 
 void drawLightRainWithSunOrMoon() {
@@ -388,40 +410,40 @@ void drawLightRainWithSunOrMoon() {
 }
 
 void drawLightSnowfall() {
-  tft.drawBitmap(0, 5, cloud, 80, 43, GREY);
-  tft.fillCircle(25, 55, 3, GREY);
-  tft.fillCircle(40, 58, 3, GREY);
-  tft.fillCircle(57, 55, 3, GREY);
+  tft.drawBitmap(0, 5, cloud, 80, 43, WHITE);
+  tft.fillCircle(25, 55, 3, WHITE);
+  tft.fillCircle(40, 58, 3, WHITE);
+  tft.fillCircle(57, 55, 3, WHITE);
 }
 
 void drawModerateSnowfall() {
-  tft.drawBitmap(0, 5, cloud, 80, 43, GREY);
-  tft.fillCircle(25, 55, 3, GREY);
-  tft.fillCircle(25, 65, 3, GREY);
-  tft.fillCircle(40, 58, 3, GREY);
-  tft.fillCircle(40, 58, 3, GREY);
-  tft.fillCircle(57, 55, 3, GREY);
-  tft.fillCircle(57, 65, 3, GREY);
+  tft.drawBitmap(0, 5, cloud, 80, 43, WHITE);
+  tft.fillCircle(25, 55, 3, WHITE);
+  tft.fillCircle(25, 65, 3, WHITE);
+  tft.fillCircle(40, 58, 3, WHITE);
+  tft.fillCircle(40, 58, 3, WHITE);
+  tft.fillCircle(57, 55, 3, WHITE);
+  tft.fillCircle(57, 65, 3, WHITE);
 }
 
 void drawHeavySnowfall() {
-  tft.drawBitmap(0, 5, cloud, 80, 43, GREY);
-  tft.fillCircle(15, 55, 3, GREY);
-  tft.fillCircle(27, 55, 3, GREY);
-  tft.fillCircle(27, 65, 3, GREY);
-  tft.fillCircle(40, 58, 3, GREY);
-  tft.fillCircle(40, 68, 3, GREY);
-  tft.fillCircle(55, 55, 3, GREY);
-  tft.fillCircle(55, 65, 3, GREY);
-  tft.fillCircle(67, 55, 3, GREY);     
+  tft.drawBitmap(0, 5, cloud, 80, 43, WHITE);
+  tft.fillCircle(15, 55, 3, WHITE);
+  tft.fillCircle(27, 55, 3, WHITE);
+  tft.fillCircle(27, 65, 3, WHITE);
+  tft.fillCircle(40, 58, 3, WHITE);
+  tft.fillCircle(40, 68, 3, WHITE);
+  tft.fillCircle(55, 55, 3, WHITE);
+  tft.fillCircle(55, 65, 3, WHITE);
+  tft.fillCircle(67, 55, 3, WHITE);     
 }
 
 void drawFog() {
-  tft.fillRoundRect(20, 10, 40, 4, 1, GREY);
-  tft.fillRoundRect(15, 20, 50, 4, 1, GREY);
-  tft.fillRoundRect(10, 30, 60, 4, 1, GREY);
-  tft.fillRoundRect(15, 40, 50, 4, 1, GREY);
-  tft.fillRoundRect(20, 50, 40, 4, 1, GREY);
+  tft.fillRoundRect(20, 10, 40, 4, 1, WHITE);
+  tft.fillRoundRect(15, 20, 50, 4, 1, WHITE);
+  tft.fillRoundRect(10, 30, 60, 4, 1, WHITE);
+  tft.fillRoundRect(15, 40, 50, 4, 1, WHITE);
+  tft.fillRoundRect(20, 50, 40, 4, 1, WHITE);
 }
 
 void showMessage(String message) {
@@ -447,13 +469,12 @@ void waitForWifiConnection() {
 }
 
 void getTemperatureData() {
-  float intTemp = dht.readTemperature();
-  float intHumidity = dht.readHumidity();
-  while (intTemp > 100) {
-    delay(1000);
-    intTemp = dht.readTemperature();
-    intHumidity = dht.readHumidity();
+  intTemp = dht.readTemperature();
+  intHumidity = dht.readHumidity();
+  if (isnan(intTemp) || intTemp < 0 || intTemp > 100) {
+    return;
   }
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setFreeFont(&ArialRoundedMTBold_14);
   tft.setTextDatum(TR_DATUM);
   tft.setTextPadding(tft.textWidth("-88`"));
@@ -508,10 +529,8 @@ void setup() {
 
   showTime();
   getWeatherData();
-  getTemperatureData();
 
   Alarm.timerRepeat(WEATHER_UPDATE_INTERVAL, getWeatherData);
-  Alarm.timerRepeat(TEMPERATURE_UPDATE_INTERVAL , getTemperatureData);
   Alarm.timerRepeat(TIME_UPDATE_INTERVAL , showTime);
   Alarm.timerRepeat(TIME_TICK_UPDATE_INTERVAL , showTimeTick);
 }
